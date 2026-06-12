ff1f84bb59527c6afbb60bcbe23dcd2f0242ae9b
FA-50 <wjdtn747@naver.com>
2026-06-12 09:44:55 +0900

    [FEAT] Git 초기 설정

---
+++ src/main/java/io/rapa/cicddemo/controller/hihi.java
@@ -0,0 +1,12 @@
+package io.rapa.cicddemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hihi {
+    @GetMapping("hello")
+    public String sayHello(){
+        return "Hello.";
+    }
+}
